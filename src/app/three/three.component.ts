9d9a2b61e36e861a5df316205c49fcede4e78a2a
sasaco <sasaicco@gmail.com>
2019-10-14 12:11:35 +0900

    mouse three.js

--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -1,28 +1,161 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { ThreeService } from './three.service';
+import {
+  Component, Input, ElementRef, OnInit, AfterViewInit, ViewChild
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { switchMap, takeUntil, pairwise } from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-three',
   templateUrl: './three.component.html',
   styleUrls: ['./three.component.scss']
 })
-export class ThreeComponent implements OnInit {
+export class ThreeComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('rendererCanvas', { static: true })
-  @HostListener('document:mousemove', ['$event'])
-    
-  public rendererCanvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvas', { static: false }) public canvas: ElementRef;
+
+  @Input() public width = 400;
+  @Input() public height = 400;
+
+  private cx: CanvasRenderingContext2D;
+  
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d');
+
+    canvasEl.width = this.width;
+    canvasEl.height = this.height;
+
+    this.cx.lineWidth = 3;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+
+    this.captureEvents(canvasEl);
+  }
+
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+    // this will capture all mousedown events from the canvas element
+    fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap((e) => {
+          // after a mouse down, we'll record all mouse moves
+          return fromEvent(canvasEl, 'mousemove')
+            .pipe(
+              // we'll stop (and unsubscribe) once the user releases the mouse
+              // this will trigger a 'mouseup' event    
+              takeUntil(fromEvent(canvasEl, 'mouseup')),
+              // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+              takeUntil(fromEvent(canvasEl, 'mouseleave')),
+              // pairwise lets us get the previous value to draw a line from
+              // the previous point to the current point    
+              pairwise()
+            )
+        })
+      )
+      .subscribe((res: [MouseEvent, MouseEvent]) => {
+        console.log('ngfgngf');
+        const rect = canvasEl.getBoundingClientRect();
+
+        // previous and current position with the offset
+        const prevPos = {
+          x: res[0].clientX - rect.left,
+          y: res[0].clientY - rect.top
+        };
+
+        const currentPos = {
+          x: res[1].clientX - rect.left,
+          y: res[1].clientY - rect.top
+        };
+
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+  }
+
+  private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
+    console.log('sdfsdfd');
+    if (!this.cx) { return; }
+
+    this.cx.beginPath();
+
+    if (prevPos) {
+      this.cx.moveTo(prevPos.x, prevPos.y); // from
+      this.cx.lineTo(currentPos.x, currentPos.y);
+      this.cx.stroke();
+    }
+  }
+
+}
+/*
+  @ViewChild('canvas', { static: true })
+  public canvas: ElementRef<HTMLCanvasElement>;
 
   constructor(private three: ThreeService) { }
 
   ngOnInit() {
-    this.three.createScene(this.rendererCanvas);
+    this.three.createScene(this.canvas);
     this.three.animate();
+    console.log('ngOnInit');
+  }
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.captureEvents(canvasEl);
+    console.log('ngAfterViewInit');
   }
 
-  onMouseMove(e) {
-    //https://ics.media/tutorial-three/raycast/
-    console.log(e);
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+
+    console.log('after a mouse down, well record all mouse moves');
+    // this will capture all mousedown events from the canvas element
+    fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap((e) => {
+          // after a mouse down, we'll record all mouse moves
+          return fromEvent(canvasEl, 'mousemove')
+            .pipe(
+              // we'll stop (and unsubscribe) once the user releases the mouse
+              // this will trigger a 'mouseup' event    
+              takeUntil(fromEvent(canvasEl, 'mouseup')),
+              // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+              takeUntil(fromEvent(canvasEl, 'mouseleave')),
+              // pairwise lets us get the previous value to draw a line from
+              // the previous point to the current point    
+              pairwise()
+            )
+        })
+      )
+      .subscribe((res: [MouseEvent, MouseEvent]) => {
+        const rect = canvasEl.getBoundingClientRect();
+        console.log('subscribe');
+        // previous and current position with the offset
+        const prevPos = {
+          x: res[0].clientX - rect.left,
+          y: res[0].clientY - rect.top
+        };
+
+        const currentPos = {
+          x: res[1].clientX - rect.left,
+          y: res[1].clientY - rect.top
+        };
+
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+  }
+
+  private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
+
+    // if (prevPos) {
+    //   //https://ics.media/tutorial-three/raycast/
+    // https://codepen.io/JoshuaMorris/pen/oXajoB
+       console.log(prevPos);
+    // }
   }
 
 }
+*/
