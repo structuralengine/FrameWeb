80034d30ace76f3d1ea3fdd0e064889943fe1d57
笹澤陽佑 <sasaicco@gmail.com>
2020-01-22 08:53:53 +0900

    force

--- src/app/components/three/geometry/three-section-force.service.ts
+++ src/app/components/three/geometry/three-section-force.service.ts
@@ -28,6 +28,7 @@ export class ThreeSectionForceService {
 
   private scale: number;
   private params: any;          // GUIの表示制御
+  private radioButtons = ['axialForce', 'shearForceY', 'shearForceZ', 'torsionalMoment', 'momentY', 'momentZ'];
   private gui: any;
 
   constructor(private scene: SceneService,
@@ -45,14 +46,12 @@ export class ThreeSectionForceService {
     this.scale = 0.001;
     // gui
     this.params = {
-      forceScale: this.scale,
-      axialForce: false,
-      shearForceY: false,
-      shearForceZ: false,
-      torsionalMoment: false,
-      momentY: true,
-      momentZ: false
+      forceScale: this.scale
     };
+    for (const key of this.radioButtons) {
+      this.params[key] = false;
+    }
+    this.params.momentY = true; // 初期値
     this.gui = null;
   }
 
@@ -77,21 +76,22 @@ export class ThreeSectionForceService {
       return;
     }
     this.gui = {
-      g1: this.scene.gui.add(this.params, 'forceScale', 0, 1).step(0.01).onChange((value) => {
+      forceScale: this.scene.gui.add(this.params, 'forceScale', 0, 1).step(0.01).onChange((value) => {
         // guiによる設定
         this.scale = value;
         this.onResize();
-      }),
-      g2: this.scene.gui.add(this.params, 'axialForce').onChange((value) => {
+      })
+    };
+    for (const key of this.radioButtons) {
+      this.scene.gui.add(this.params, key, this.params[key]).listen().onChange((value) => {
         if ( value === true ) {
-
+          this.setGUIcheck(key);
         } else {
-
+          this.setGUIcheck('');
         }
         this.onResize();
-      }),
-
-    };
+      });
+    }
   }
 
   public guiDisable(): void {
@@ -99,11 +99,20 @@ export class ThreeSectionForceService {
       return;
     }
     for ( const key of Object.keys(this.gui)) {
-      this.scene.gui.remove(this.gui(key));
+      this.scene.gui.remove(this.gui[key]);
     }
     this.gui = null;
   }
 
+  // gui 選択されたチェックボックス以外をOFFにする
+  private setGUIcheck(target: string): void {
+    for (const key of this.radioButtons) {
+      this.params[key] = false;
+    }
+    this.params[target] = true;
+  }
+
+  // データが変更された時に呼び出される
   public chengeData(index: number): void {
 
     // 格点データを入手
@@ -143,7 +152,7 @@ export class ThreeSectionForceService {
 
       // 部材の座標軸を取得
       const localAxis = this.three_member.localAxis(i.x, i.y, i.z, j.x, j.y, j.z, m.cg);
-      const MemberLength = this.member.getMemberLength(id);
+      const MemberLength: number = Math.sqrt((i.x - j.x) ** 2 + (i.y - j.y) ** 2 + (i.z - j.z) ** 2);
 
       // 着目点
       const fsecPoints: any = new Array();
@@ -151,12 +160,23 @@ export class ThreeSectionForceService {
       for (const fsec of fsecData ) {
         if ( fsec.m === id ) {
           flg = true;
-          fsecPoints.push(this.getFsecPoints(id, MemberLength, fsec, i, j, localAxis));
+          // 1つめのデータは部材情報
+          fsecPoints.push({
+            id: fsec.m ,
+            iPosition: i,
+            jPosition: j,
+            length: MemberLength,
+            localAxisX: localAxis.x,
+            localAxisY: localAxis.y,
+            localAxisZ: localAxis.z
+          });
+          // ２つめのデータ以降が断面力情報
+          fsecPoints.push(this.getFsecPoints(MemberLength, fsec, i, j));
         } else if (flg === true) {
           if ( fsec.m.trim().length > 0 ) {
             break;
           }
-          fsecPoints.push(this.getFsecPoints(id, MemberLength, fsec, i, j, localAxis));
+          fsecPoints.push(this.getFsecPoints(MemberLength, fsec, i, j));
         }
       }
       this.targetData.push(fsecPoints);
@@ -167,10 +187,9 @@ export class ThreeSectionForceService {
   }
 
   // 断面力, 部材の情報をまとめる
-  private getFsecPoints(MemberNo, MemberLength, fsec, i, j, localAxis) {
+  private getFsecPoints(MemberLength, fsec, i, j) {
 
-    const localPosition: number = this.helper.toNumber(fsec.l);
-
+    // 断面力の集計
     const axialForce: number = this.helper.toNumber(fsec.fx);
     const shearForceY: number = this.helper.toNumber(fsec.fy);
     const shearForceZ: number = this.helper.toNumber(fsec.fz);
@@ -178,13 +197,15 @@ export class ThreeSectionForceService {
     const momentY: number = this.helper.toNumber(fsec.my);
     const momentZ: number = this.helper.toNumber(fsec.mz);
 
+    // 位置
+    const localPosition: number = this.helper.toNumber(fsec.l);
     const worldPosition = {
       x: (localPosition / MemberLength) * ( j.x - i.x ) + i.x,
       y: (localPosition / MemberLength) * ( j.y - i.y ) + i.y,
       z: (localPosition / MemberLength) * ( j.z - i.z ) + i.z
     };
 
-    const forceInfo = {
+    return {
       worldPosition,
       localPosition,
       axialForce,
@@ -195,17 +216,6 @@ export class ThreeSectionForceService {
       momentZ
     };
 
-    const memberInfo = {
-      id: MemberNo,
-      iPosition: i,
-      jPosition: j,
-      length: MemberLength,
-      localAxisX: localAxis.x,
-      localAxisY: localAxis.y,
-      localAxisZ: localAxis.z
-    };
-
-    return {memberInfo, forceInfo};
   }
 
   // 断面力図を描く
@@ -215,51 +225,58 @@ export class ThreeSectionForceService {
     this.ClearData();
 
     for (const target of this.targetData) {
+      // 1つめのデータは部材情報
+      const memberInfo: object = target[0];
+      // ２つめのデータ以降が断面力情報
+      for (let i = 1; i < target.length; i++) {
+        const force: object = target[i];
 
-      let xi: number = target.xi;
-      let yi: number = target.yi;
-      let zi: number = target.zi;
+        let x: number = target.worldPosition.x;
+        let y: number = target.worldPosition.y;
+        let z: number = target.worldPosition.z;
+/*
+        x += target. * this.scale;
+        y += target.dyi * this.scale;
+        z += target.dzi * this.scale;
 
-      xi += target.dxi * this.scale;
-      yi += target.dyi * this.scale;
-      zi += target.dzi * this.scale;
+        let xj: number = target.xj;
+        let yj: number = target.yj;
+        let zj: number = target.zj;
 
-      let xj: number = target.xj;
-      let yj: number = target.yj;
-      let zj: number = target.zj;
+        xj += target.dxj * this.scale;
+        yj += target.dyj * this.scale;
+        zj += target.dzj * this.scale;
 
-      xj += target.dxj * this.scale;
-      yj += target.dyj * this.scale;
-      zj += target.dzj * this.scale;
+        const positions = [];
+        positions.push(xi, yi, zi);
+        positions.push(xj, yj, zj);
 
-      const positions = [];
-      positions.push(xi, yi, zi);
-      positions.push(xj, yj, zj);
+        const threeColor = new THREE.Color(0xFF0000);
+        const colors = [];
+        colors.push(threeColor.r, threeColor.g, threeColor.b);
+        colors.push(threeColor.r, threeColor.g, threeColor.b);
 
-      const threeColor = new THREE.Color(0xFF0000);
-      const colors = [];
-      colors.push(threeColor.r, threeColor.g, threeColor.b);
-      colors.push(threeColor.r, threeColor.g, threeColor.b);
+        const geometry: LineGeometry = new LineGeometry();
+        geometry.setPositions(positions);
+        geometry.setColors(colors);
 
-      const geometry: LineGeometry = new LineGeometry();
-      geometry.setPositions(positions);
-      geometry.setColors(colors);
+        const matLine: LineMaterial = new LineMaterial({
+          color: 0xFF0000,
+          linewidth: 0.001,
+          vertexColors: THREE.VertexColors,
+          dashed: false
+        });
+        const line: Line2 = new Line2(geometry, matLine);
+        line.computeLineDistances();
 
-      const matLine: LineMaterial = new LineMaterial({
-        color: 0xFF0000,
-        linewidth: 0.001,
-        vertexColors: THREE.VertexColors,
-        dashed: false
-      });
-      const line: Line2 = new Line2(geometry, matLine);
-      line.computeLineDistances();
+        line.scale.set(1, 1, 1);
+        line.name = target.name;
 
-      line.scale.set(1, 1, 1);
-      line.name = target.name;
+        this.lineList.push(line);
 
-      this.lineList.push(line);
-
-      this.scene.add(line);
+        this.scene.add(line);
+      */
+      }
     }
   }
 }
