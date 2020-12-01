cc1f4afc6b7a7bc6fb383cb7f3152880f58f4f38
mizutani488 <g1918082@tcu.ac.jp>
2020-12-01 21:28:54 +0900

    ページの数字表示まで

--- src/app/components/input/input-define/input-define.component.ts
+++ src/app/components/input/input-define/input-define.component.ts
@@ -3,7 +3,10 @@ import { InputDefineService } from './input-define.service';
 import { InputLoadService } from '../input-load/input-load.service';
 import { ResultDataService } from '../../../providers/result-data.service';
 import { DataHelperModule } from '../../../providers/data-helper.module';
-import{ UserInfoService } from '../../../providers/user-info.service'
+import { UserInfoService } from '../../../providers/user-info.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-input-define',
@@ -11,13 +14,26 @@ import{ UserInfoService } from '../../../providers/user-info.service'
   styleUrls: ['./input-define.component.scss', '../../../app.component.scss']
 })
 export class InputDefineComponent implements OnInit, AfterViewInit {
+  message: string;
+  myControl: FormGroup;
+  number2: string;
 
   ROWS_COUNT = 20;
   COLUMNS_COUNT: number;
   page: number;
+  page_1: number;
+  page_2: number;
+  page11: number;
+  page12: number;
+  page0: number;
   defineData: any[];
   defineColums: any[];
   rowHeaders: any[];
+  key = "";
+
+  show(e: any) {
+    this.key = e.key;
+  }
 
   hotTableSettings = {
     beforeChange: (...x: any[]) => {
@@ -32,8 +48,8 @@ export class InputDefineComponent implements OnInit, AfterViewInit {
         if (changes === undefined) { return; }
         for (let i = 0; i < changes.length; i++) {
           const value: number = this.helper.toNumber(changes[i][3]);
-          if( value !== null ) {
-                changes[i][3] = value.toFixed(0);
+          if (value !== null) {
+            changes[i][3] = value.toFixed(0);
           } else {
             changes[i][3] = null;
           }
@@ -43,7 +59,7 @@ export class InputDefineComponent implements OnInit, AfterViewInit {
       }
     },
     afterChange: (...x: any[]) => {
-      if (this.initialFlg===true){
+      if (this.initialFlg === true) {
         return;
       }
       let changes: any = undefined;
@@ -61,12 +77,17 @@ export class InputDefineComponent implements OnInit, AfterViewInit {
 
   private initialFlg = true;
   constructor(private input: InputDefineService,
-              private load: InputLoadService,
-              private result: ResultDataService,
-              private helper: DataHelperModule,
-              public user:UserInfoService,) {
+    private load: InputLoadService,
+    private result: ResultDataService,
+    private helper: DataHelperModule,
+    public user: UserInfoService,) {
 
-    this.page = 1;
+    this.page0 = 3;
+    this.page11 = 4;
+    this.page12 = 5;
+    this.page_1 = 2;
+    this.page_2 = 1;
+
     this.defineData = new Array();
     this.defineColums = new Array();
     this.rowHeaders = new Array();
@@ -83,7 +104,23 @@ export class InputDefineComponent implements OnInit, AfterViewInit {
     }
 
     this.loadPage(1);
+
+    this.message = 'please select button.';
+    this.myControl = new FormGroup({
+      number2: new FormControl(),
+    });
+
+
   }
+
+  click() {
+    const value: number = this.helper.toNumber(this.myControl.value.number2);
+    if (value !== null) {
+      this.loadPage(value);
+    }
+  }
+
+
   ngAfterViewInit() {
     this.initialFlg = false;
   }
@@ -96,7 +133,22 @@ export class InputDefineComponent implements OnInit, AfterViewInit {
   loadPage(currentPage: number) {
     if (currentPage !== this.page) {
       this.page = currentPage;
+      if (currentPage > 2) {
+        this.page0 = currentPage;
+        this.page_1 = currentPage - 1;
+        this.page_2 = currentPage - 2;
+        this.page11 = currentPage + 1;
+        this.page12 = currentPage + 2;
+      } else {
+        this.page0 = 3;
+        this.page_1 = 2;
+        this.page_2 = 1;
+        this.page11 = 4;
+        this.page12 = 5;
+      }
     }
+
+
     this.defineData = new Array();
     this.rowHeaders = new Array();
 
