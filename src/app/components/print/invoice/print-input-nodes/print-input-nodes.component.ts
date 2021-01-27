4f1a3526429aa75f045c44bcfcc732b6eb90a978
mizutani488 <g1918082@tcu.ac.jp>
2021-01-27 20:24:25 +0900

    nodeの印刷ほぼ完成

--- src/app/components/print/invoice/print-input-nodes/print-input-nodes.component.ts
+++ src/app/components/print/invoice/print-input-nodes/print-input-nodes.component.ts
@@ -26,7 +26,7 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
   invoiceDetails: Promise<any>[];
 
   public node_dataset = [];
-  public node_splid = [];
+  public node_page = [];
 
   public judge: boolean;
 
@@ -41,7 +41,7 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
     if ("node" in inputJson) {
       const tables = this.printNode(inputJson);
       this.node_dataset = tables.splid;
-      this.node_splid = tables.m;
+      this.node_page = tables.page;
       // if (tables.splid > 0) {
       //   this.node_splid = tables.splid;
       // } else {
@@ -56,91 +56,56 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
   // 格子点データ node を印刷する
   private printNode(inputJson): any {
     const minCount: number = 52; // これ以上なら２行書きとする
-    const body: any = [];
+    let body: any = [];
     const splid: any = [];
     let m: number = 0;
+    let page: number = 0;
     const json: {} = inputJson["node"]; // inputJsonからnodeだけを取り出す
     const keys: string[] = Object.keys(json);
 
     let head: string[];
 
-    if (keys.length <= minCount) {
-      head = ["No.", "X(m)", "Y(m)", "Z(m)"];
-      for (const index of keys) {
-        const item = json[index]; // 1行分のnodeデータを取り出す
-        // 印刷する1行分のリストを作る
-        const line: string[] = new Array();
-        line.push(index);
-        line.push(item.x.toFixed(3));
-        line.push(item.y.toFixed(3));
-        line.push(item.z.toFixed(3));
-        body.push(line);
-      }
-    } else if (minCount < keys.length && keys.length <= minCount * 2) {
-      // 2列表示
-      head = ["No.", "X(m)", "Y(m)", "Z(m)", "No.", "X(m)", "Y(m)", "Z(m)"];
-      const n = Math.ceil(53); // 分割位置
-      for (let i = 0; i < n; i++) {
-        const line: string[] = new Array();
-        // 左側
-        const index1: string = keys[i];
+    let break_flg = true;
+    while (break_flg) {
+      for (let i = 0; i < 54; i++) {
+        const line = ["", "", "", "", "", "", "", ""];
+
+        const j = page * 108 + i; //0, 108, 216, ...
+        if(j===289){
+          console.log('')
+        }
+        if (j > keys.length) {
+          break_flg = false;
+          break;
+        }
+        const index1: string = keys[j];
         const item1 = json[index1];
-        line.push(index1);
-        line.push(item1.x.toFixed(3));
-        line.push(item1.y.toFixed(3));
-        line.push(item1.z.toFixed(3));
-        // 右側
-        if (keys.length > n + i) {
-          const index2: string = keys[n + i];
+        line[0] = index1;
+        line[1] = item1.x.toFixed(3);
+        line[2] = item1.y.toFixed(3);
+        line[3] = item1.z.toFixed(3);
+
+        const k = j + 54;
+        if (k < keys.length) {
+          const index2: string = keys[k];
           const item2 = json[index2];
-          line.push(index2);
-          line.push(item2.x.toFixed(3));
-          line.push(item2.y.toFixed(3));
-          line.push(item2.z.toFixed(3));
-        } else {
-          line.push("");
-          line.push("");
-          line.push("");
-          line.push("");
+          line[4] = index2;
+          line[5] = item2.x.toFixed(3);
+          line[6] = item2.y.toFixed(3);
+          line[7] = item2.z.toFixed(3);
         }
         body.push(line);
       }
-    } else {
-      // 2列表示.ページがまたぐ場合
-      console.log("keyslength",keys.length);
-      m = Math.ceil(keys.length / 107);
-      for (let j = 1; j <= m; j++) {
-        const n = Math.ceil(161 * j - 108); // 分割位置
-        const f = 108 * j - 107; // 分割した後の初項となる値を調べる
-        for (let i = f; i <= n; i++) {
-          const line: string[] = new Array();
-          // 左側
-          const index1: string = keys[i];
-          const item1 = json[index1];
-          line.push(index1);
-          line.push(item1.x.toFixed(3));
-          line.push(item1.y.toFixed(3));
-          line.push(item1.z.toFixed(3));
-          // 右側
-          if (keys.length > minCount * 2) {
-            const index2: string = keys[n + 1];
-            const item2 = json[index2];
-            line.push(index2);
-            line.push(item2.x.toFixed(3));
-            line.push(item2.y.toFixed(3));
-            line.push(item2.z.toFixed(3));
-          } else {
-            line.push("");
-            line.push("");
-            line.push("");
-            line.push("");
-          }
-          body.push(line);
-        }
-        splid.push(body);
+      
+      if(body.length === 0){
+        break;
       }
+      splid.push(body);
+      body = [];
+      page++;
     }
+
     this.countTotal = (keys.length + 1) * 20 + 40;
-    return { m, splid, this: this.countTotal };
+    return { page, splid, this: this.countTotal };
   }
 }
