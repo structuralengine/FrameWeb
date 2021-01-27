3cd7cc82755660098a2e5cf727aa6ed7b611f074
mizutani488 <g1918082@tcu.ac.jp>
2021-01-27 18:03:40 +0900

    格点データの実験

--- src/app/components/print/invoice/print-input-nodes/print-input-nodes.component.ts
+++ src/app/components/print/invoice/print-input-nodes/print-input-nodes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { InputDataService } from "../../../../providers/input-data.service";
 import { AfterViewInit } from "@angular/core";
 import { DataCountService } from "../dataCount.service";
+// import { table } from "console";
 
 @Component({
   selector: "app-print-input-nodes",
@@ -25,6 +26,7 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
   invoiceDetails: Promise<any>[];
 
   public node_dataset = [];
+  public node_splid = [];
 
   public judge: boolean;
 
@@ -39,6 +41,11 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
     if ("node" in inputJson) {
       const tables = this.printNode(inputJson);
       this.node_dataset = tables.body;
+      if(tables.splid > 0){
+      this.node_splid = tables.splid;
+      }else{
+        this.node_splid = [1];
+      }
       this.judge = this.countArea.setCurrentY(tables.this);
     }
   }
@@ -47,14 +54,15 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
 
   // 格子点データ node を印刷する
   private printNode(inputJson): any {
-    const minCount: number = 5; // これ以上なら２行書きとする
+    const minCount: number = 52; // これ以上なら２行書きとする
     const body: any = [];
+    const splid: any = [];
     const json: {} = inputJson["node"]; // inputJsonからnodeだけを取り出す
     const keys: string[] = Object.keys(json);
 
     let head: string[];
 
-    if (keys.length < minCount) {
+    if (keys.length <= minCount) {
       head = ["No.", "X(m)", "Y(m)", "Z(m)"];
       for (const index of keys) {
         const item = json[index]; // 1行分のnodeデータを取り出す
@@ -66,10 +74,10 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
         line.push(item.z.toFixed(3));
         body.push(line);
       }
-    } else {
+    } else if (minCount < keys.length && keys.length <= minCount * 2) {
       // 2列表示
       head = ["No.", "X(m)", "Y(m)", "Z(m)", "No.", "X(m)", "Y(m)", "Z(m)"];
-      const n = Math.ceil(keys.length / 2); // 分割位置
+      const n = Math.ceil(53); // 分割位置
       for (let i = 0; i < n; i++) {
         const line: string[] = new Array();
         // 左側
@@ -95,8 +103,40 @@ export class PrintInputNodesComponent implements OnInit, AfterViewInit {
         }
         body.push(line);
       }
+    } else {
+      // 2列表示.ページがまたぐ場合
+      const m = Math.ceil(keys.length / 106);
+      for (let j = 0; j < m; j++) {
+        const n = Math.ceil(106 * m - 52); // 分割位置
+        for (let i = 0; i < n; i++) {
+          const line: string[] = new Array();
+          // 左側
+          const index1: string = keys[i];
+          const item1 = json[index1];
+          line.push(index1);
+          line.push(item1.x.toFixed(3));
+          line.push(item1.y.toFixed(3));
+          line.push(item1.z.toFixed(3));
+          // 右側
+          if (keys.length > n + i) {
+            const index2: string = keys[n + i];
+            const item2 = json[index2];
+            line.push(index2);
+            line.push(item2.x.toFixed(3));
+            line.push(item2.y.toFixed(3));
+            line.push(item2.z.toFixed(3));
+          } else {
+            line.push("");
+            line.push("");
+            line.push("");
+            line.push("");
+          }
+          body.push(line);
+        }
+        splid.push(body);
+      }
     }
     this.countTotal = (keys.length + 1) * 20 + 40;
-    return { body, this: this.countTotal };
+    return { splid, body, this: this.countTotal };
   }
 }
