a13ef39421f4b86c46411b41c81432210bec69ab
mizutani488 <g1918082@tcu.ac.jp>
2021-01-29 12:15:33 +0900

    部材まで印刷成功した

--- src/app/components/print/invoice/print-input-members/print-input-members.component.ts
+++ src/app/components/print/invoice/print-input-members/print-input-members.component.ts
@@ -3,7 +3,6 @@ import { InputDataService } from "../../../../providers/input-data.service";
 import { AfterViewInit } from "@angular/core";
 import { DataCountService } from "../dataCount.service";
 
-
 @Component({
   selector: "app-print-input-members",
   templateUrl: "./print-input-members.component.html",
@@ -14,7 +13,6 @@ import { DataCountService } from "../dataCount.service";
   ],
 })
 export class PrintInputMembersComponent implements OnInit, AfterViewInit {
-  page: number;
   load_name: string;
   collectionSize: number;
   countCell: number;
@@ -26,8 +24,10 @@ export class PrintInputMembersComponent implements OnInit, AfterViewInit {
   invoiceDetails: Promise<any>[];
 
   public member_dataset = [];
+  public member_page = [];
 
   public judge: boolean;
+  public judgeLast: boolean;
 
   constructor(
     private InputData: InputDataService,
@@ -39,8 +39,10 @@ export class PrintInputMembersComponent implements OnInit, AfterViewInit {
 
     if ("member" in inputJson) {
       const tables = this.printMember(inputJson);
-      this.member_dataset = tables.body;
+      this.member_dataset = tables.splid;
+      this.member_page = tables.page;
       this.judge = this.countArea.setCurrentY(tables.this);
+      this.judgeLast = this.countArea.setCurrentY(tables.lastArrayCount);
     }
   }
 
@@ -48,24 +50,62 @@ export class PrintInputMembersComponent implements OnInit, AfterViewInit {
 
   //要素データ member を印刷する
   private printMember(inputJson): any {
-    const body: any = [];
+    let body: any = [];
+    const splid: any = [];
+    let page: number = 0;
     const json: {} = inputJson["member"]; // inputJsonからnodeだけを取り出す
     const keys: string[] = Object.keys(json);
 
-    for (const index of keys) {
-      const item = json[index]; // 1行分のnodeデータを取り出す
-      const len: number = this.InputData.member.getMemberLength(index); // 部材長さ
-      // 印刷する1行分のリストを作る
-      const line: string[] = new Array();
-      line.push(index);
-      line.push(item.ni.toString());
-      line.push(item.nj.toString());
-      line.push(len.toFixed(3));
-      line.push(item.e.toString());
-      line.push(item.cg.toString());
-      body.push(line);
+    let break_flg = true;
+
+    while (break_flg) {
+      for (let i = 0; i < 59; i++) {
+        const line = ["", "", "", "", "", ""];
+        let index: string = keys[i];
+        const item = json[index]; // 1行分のnodeデータを取り出す
+        const len: number = this.InputData.member.getMemberLength(index); // 部材長さ
+        const j = page * 59 + i + 1;
+        const s = j + 1;
+
+        if (s > keys.length) {
+          break_flg = false;
+          break;
+        }
+
+        line[0] = index;
+        line[1] = item.ni.toString();
+        line[2] = item.nj.toString();
+        line[3] = len.toFixed(3);
+        line[4] = item.e.toString();
+        line[5] = item.cg.toString();
+
+        body.push(line);
+      }
+
+      if (body.length === 0) {
+        break;
+      }
+      splid.push(body);
+      body = [];
+      page++;
     }
+
+    // for (const index of keys) {
+    //   const item = json[index]; // 1行分のnodeデータを取り出す
+    //   const len: number = this.InputData.member.getMemberLength(index); // 部材長さ
+    //   // 印刷する1行分のリストを作る
+    //   const line: string[] = new Array();
+    //   line.push(index);
+    //   line.push(item.ni.toString());
+    //   line.push(item.nj.toString());
+    //   line.push(len.toFixed(3));
+    //   line.push(item.e.toString());
+    //   line.push(item.cg.toString());
+    //   body.push(line);
+    // }
+    const lastArray = splid.slice(-1)[0];
+    const lastArrayCount = lastArray.length;
     this.countTotal = (keys.length + 1) * 20 + 40;
-    return {body,this:this.countTotal};
+    return { page, splid, this: this.countTotal, lastArrayCount };
   }
 }
