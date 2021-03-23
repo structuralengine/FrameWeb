996cdc6196aace470218bd2e0e434d68c84463fa
sasaco <sasaicco@gmail.com>
2021-03-23 13:27:58 +0900

    install firebase

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { AuthService } from './auth.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
